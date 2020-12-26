085e4833a713c5301ce812c7ef08ee56a05cb173
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-12-26 22:34:23 +0200

    update

--- Agurkai/skynimas.php
+++ Agurkai/skynimas.php
@@ -5,21 +5,35 @@ if (!isset($_SESSION['a'])) {
     $_SESSION['a'] = [];
     $_SESSION['agurku ID'] = 0;
 }
+
 //SKINTI SCENARIJUS
 if (isset($_POST['skinti'])) {
     foreach($_SESSION['a'] as $index => &$agurkas) {
-        $agurkas['agurkai'] -= $_POST['kiek_skinti'];
-        print_r($_POST);
+            $skynimas = $_POST['kiek_skinti'];
+                $agurkas['agurkai'] -= $_POST['kiek_skinti'][$agurkas['id']];
     }
-
     header('Location: http://localhost/BIT_KURSAI_PHP/Pirmas/Agurkai/skynimas.php.');
     exit;
-   
 }
 
 
 //SKINTI VISUS SCENARIJUS
+if (isset($_POST['skinti_visus'])) {
+    foreach($_SESSION['a'] as $index => &$agurkas) {
+        $agurkas['agurkai'] = 0;
+        header('Location: http://localhost/BIT_KURSAI_PHP/Pirmas/Agurkai/skynimas.php.');
+        exit;
+    }
+}
 //NUIMTI VISA DERLIU SCENARIJUS
+
+if (isset($_POST['nuimtiDerliu'])) {
+    foreach($_SESSION['a'] as $index => &$agurkas) {
+        $agurkas['agurkai'] = 0;
+        header('Location: http://localhost/BIT_KURSAI_PHP/Pirmas/Agurkai/skynimas.php.');
+        exit;
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -38,12 +52,12 @@ if (isset($_POST['skinti'])) {
     Agurkas Nr. <?= $agurkas['id'] ?>
     <p style="display:inline;">Galima skinti</p>
     <h1 style="display:inline;"><?= $agurkas['agurkai'] ?></h1>
-    <input type="text" name="kiek_skinti">
-    <button type="submit" name="Skinti">Skinti</button>
-    <button type="submit" name="Skinti visus">Skinti visus</button>
+    <input type="text" name="kiek_skinti[<?= $agurkas['id'] ?>]"value=<?= $skynimas ?? 0 ?>>
+    <button type="submit" name="skinti">Skinti</button>
+    <button type="submit" name="skinti_visus">Skinti visus</button>
     </div>
     <?php endforeach ?>
-    <button type="submit" name="NuimtiDerliu">Nuimti visą derlių</button>
+    <button type="submit" name="nuimtiDerliu">Nuimti visą derlių</button>
     </form>
 </body>
 </html>
